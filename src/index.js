dfd6053dda3fd4a52ea98c02d740333b05ff3e2a
rishika <rishikasoni2827@gmail.com>
2024-07-31 18:11:00 +0600

    Rendered React Component on DOM Node

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App'; // Ensure this matches the file name and path
 
 
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
